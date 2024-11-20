83fb5348d0858b4aa95f60eee4714861fc6d7ba6
Stanislaw Malinowski <stanislaw.malinowski@diamond.ac.uk>
2024-11-20 14:08:35 +0000

    tutorial finished

--- vite-project/api/main.ts
+++ vite-project/api/main.ts
@@ -1,6 +1,7 @@
 import { Application, Router } from "@oak/oak";
 import { oakCors } from "@tajpouria/cors";
 import data from "./data.json" with { type: "json" };
+import routeStaticFilesFrom from "./utils/routeStaticFilesFrom.ts";
 
 const router = new Router();
 
@@ -24,5 +25,9 @@ const app = new Application();
 app.use(oakCors());
 app.use(router.routes());
 app.use(router.allowedMethods());
+app.use(routeStaticFilesFrom( [
+    `${Deno.cwd()}/dist`,
+    `${Deno.cwd()}/public`,
+]));
 
-await app.listen({ port: 8000 });
+await app.listen({ port: 8001 });
